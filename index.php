6f9f68e80445434d9d4d3a5c1920f04025f2bd18
Luter <lutersergei@mail.ru>
2016-05-26 15:30:18 +0700

    Добавлено перенаправление на регистрацию, если пустая сессия и куки

--- index.php
+++ index.php
@@ -1,59 +1,14 @@
 <?php
-
-define("TIME_YEAR" , 3600*24*365);
-define("USER_GUEST", 'Гость');
-
-if (isset($_POST['action']))
+if (isset($_SESSION['login']))
 {
-    if (($_POST['action']=='login')&&(!isset($_COOKIE['name'])))
-    {
-        setcookie("name", md5(time()), time() + TIME_YEAR);
-        header("Location:/");
-        die();
-    }
-    if (($_POST['action']=='logout'))
-    {
-        setcookie("name", "", time() - 60);
-        header("Location:/");
-        die();
-    }
-}
-if (isset($_COOKIE['name']))
-{
-    $name = $_COOKIE['name'];
-
+    
 }
 else
 {
-    $name = USER_GUEST;
+    if (isset($_COOKIE['login']))
+    {
+        $_SESSION['login'] = $_COOKIE['login'];
+    }
+    else header("Location: registration.php");
 }
 ?>
-
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-<h3>Привет, <?php echo $name?></h3>
-<?php if ($name===USER_GUEST)
-{ ?>
-    <form method="post">
-        <input type="hidden" name="action" value="login"/>
-        <input type="submit" value="Войти"/>
-    </form>
-    <?php
-}
-
-else
-{ ?>
-    <form method="post">
-        <input type="hidden" name="action" value="logout"/>
-        <input type="submit" value="Выйти"/>
-    </form>
-<?php
-}
-?>
-</body>
-</html>
